c13b80dcd2c1e088420ee243b92f28a140914f04
Javad Rafiei <javadrafiei@Javads-MacBook-Air.local>
2024-11-05 22:46:14 +0330

    Added number to presale.

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -261,9 +261,9 @@ function HeroSection() {
 
       const totalStages = Number(totalStagesRaw?.toString());
 
-      let totalSoldToken = (6800000 + 4289000);
+      let totalSoldToken = (6800000 + 8089000);
       let totalTokenToSell = 0;
-      let currentStageSoldToken = 4289000;
+      let currentStageSoldToken = 8089000;
       let currentStageTokenSell = 0;
 
       for (let i = 0; i < totalStages; i++) {
