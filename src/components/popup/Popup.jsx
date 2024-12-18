1bd0c09f5a1690b67c7117c747150ef0248b148e
captain-hook-web <captainhook.game@gmail.com>
2024-12-18 10:22:15 +0330

    Update Popup.jsx

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -10,13 +10,13 @@ function Popup({ onClosePopup, popup }) {
             <div className="relative bg-popup bg-no-repeat bg-[length:100%_100%] w-full max-w-[500px] h-auto pt-20 sm:pt-28 sm:pt-32  mt-10 scale-[.7] xl:scale-[1]">
                 <img src={Vip} alt="Vip Club" loading="lazy" className="w-[100%] absolute left-0 top-[-120px]" />
                 <h4 className="title-popup w-[85%] mx-auto text-white text-center text-[18px]">🎯 Exclusive Presale Stage 2 Offer: More Tokens, More Rewards! 🎯</h4>
-                <p className="w-[80%] mx-auto open-sans-Medium text-white text-center text-[16px] my-2">🔹 Special rewards for Stage 1 presale investors:</p>
+                <p className="w-[80%] mx-auto open-sans-Medium text-white text-center text-[16px] my-2">🔹 Special rewards for Stage 2 presale investors:</p>
                 {/* <div className="w-[98%] mx-auto bg-[#00000060] py-2">
                     <div id="timer" className="w-[70%] mx-auto">
                         <CountDownTimer time={1730937540} />
                     </div>
                 </div> */}
-                <h4 className="open-sans-Medium w-[85%] mx-auto text-white text-[14px] my-2 text-center">The more you buy, the more bonus tokens you’ll receive in this early stage!</h4>
+                <h4 className="open-sans-Medium w-[85%] mx-auto text-white text-[14px] my-2 text-center"> more buy, more bonus!</h4>
                 <ul className="list-popup w-[70%] mx-auto text-white text-[18px] my-5">
                     <li><span>1.</span> <span className="text-lg">💰</span> Over $100: 10% bonus tokens</li>
                     <li><span>2.</span> <span className="text-lg">💰</span> Over $200: 20% bonus tokens</li>
@@ -25,8 +25,8 @@ function Popup({ onClosePopup, popup }) {
                     <li><span>5.</span> <span className="text-lg">📣</span> Early Access to News and Announcements!</li>
                     <li><span>6.</span> <span className="text-lg">🏅</span> Special Participation Badges!</li> */}
                 </ul>
-                <p className="w-[85%] mx-auto open-sans-Medium text-white text-[16px] mt-2 text-center">🌟 Stage 1 is your chance to invest early and earn maximum rewards!</p>
-                <div className="flex justify-center my-5">
+                // <p className="w-[85%] mx-auto open-sans-Medium text-white text-[16px] mt-2 text-center">🌟 Stage 1 is your chance to invest early and earn maximum rewards!</p>
+                // <div className="flex justify-center my-5">
                     <button onClick={onClosePopup} id="btn-popup" className="open-sans-Medium font-black text-lgg text-white bg-purple py-2 px-10 rounded-xl border-[1.5px] border-white transition-[1s] hover:-translate-y-1">OK</button>
                 </div>
             </div>
