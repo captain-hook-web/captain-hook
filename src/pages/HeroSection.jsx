3dbb229a7ebe5b834b1725f537d20e671b6df709
captain-hook-web <captainhook.game@gmail.com>
2024-12-23 11:05:14 +0330

    Update HeroSection.jsx

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -270,9 +270,9 @@ function HeroSection() {
 
       const totalStages = Number(totalStagesRaw?.toString());
 
-      let totalSoldToken = 11669225;
+      let totalSoldToken = 11559225;
       let totalTokenToSell = 0;
-      let currentStageSoldToken = 4170270;
+      let currentStageSoldToken = 4270270;
       let currentStageTokenSell = 0;
 
       for (let i = 0; i < totalStages; i++) {
