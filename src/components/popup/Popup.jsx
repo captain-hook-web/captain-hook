580654ecff3edd1857830a4cd8c0541a9bda70e6
captain-hook-web <captainhook.game@gmail.com>
2025-01-09 11:24:16 +0330

    Update Popup.jsx

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -4,7 +4,9 @@ import "./style.css";
 import { useEffect, useState } from "react";
 
 function Popup({ onClosePopup, popup }) {
-
+    const [popup, setPopup] = useState(false); // مقدار اولیه false
+    const handleClosePopup = () => setPopup(false);
+    
     return (
         <div className={`${popup ? "flex items-center justify-center w-full h-[100vh] bg-[#0b1023e6] fixed top-0 left-0 z-[1500]" : "hidden"}`}>
             <div className="relative bg-popup bg-no-repeat bg-[length:100%_100%] w-full max-w-[500px] h-auto pt-20 sm:pt-28 sm:pt-32  mt-10 scale-[.7] xl:scale-[1]">
