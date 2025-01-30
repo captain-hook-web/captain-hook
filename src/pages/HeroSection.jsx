3351aa6f5544b5f3411a71426f160c00b0cb8b0f
captain-hook-web <captainhook.game@gmail.com>
2025-01-30 13:07:41 +0330

    Update HeroSection.jsx

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -609,7 +609,7 @@ function HeroSection() {
                     alignItems: "center",
                   }}
                 >
-                  <CountDownTimer time={1738195165} />
+                  <CountDownTimer time={1739231959} />
                 </Stack>
                 {<Box
                   mt={2}
